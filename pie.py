183e87d329b7231359b88dd6e25a688a212bd38a
Jérémy Galland <contact@jeremy-galland.ch>
2017-03-21 20:58:52 +0100

    update README

--- pie.py
+++ pie.py
@@ -5,5 +5,5 @@ sizes = [1000,2000,3000,4000]
 colors = ['blue', 'red', 'purple', 'green']
 
 plt.pie(sizes, labels=labels, colors=colors)
-plt.title("Number of programers by language")
+plt.title("Number of programmers by language")
 plt.show()
